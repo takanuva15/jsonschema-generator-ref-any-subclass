76111c2adb5a48061587c22f661b22fdb1f95a4b
takanuva15 <takanuva15@users.noreply.github.com>
2023-11-10 16:10:47 -0500

    feat: Add attempt to use Jackson module

--- src/main/java/org/example/model/Dog.java
+++ src/main/java/org/example/model/Dog.java
@@ -1,9 +1,11 @@
 package org.example.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Dog extends Animal {
+	@JsonProperty("bark_volume")
 	private int barkVolume;
 
 	public String getBreed() {
